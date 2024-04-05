41e1531748394b9ae72879d86610bf6ca37dd418
Jwad Aziz <miike.wazzowski@gmail.com>
2024-04-05 01:29:29 -0700

    testing index.html's priority to be served if it exists in the static asset directory

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
+
+// express will prioritize index.html at the GET / route
+// if it exists in this directory, even if another 
+// definition exists
+app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
     res.send("hello, world!");
